ceaf50dc6bc5055ab5a43979cb96c2e2ac2f5805
Surendar2005 <162149722+Surendar2005@users.noreply.github.com>
2024-05-22 10:09:41 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,12 @@
 #write a program to find the factorial of a nummber
+def factorial(n):
+if n==0:
+  return 1
+else:
+  return n*factorial(n-1)
+num=int(input("Enter the number :"))
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    fact = factorial(num)
+    print(f"The factorial of {num} is {fact}.")
