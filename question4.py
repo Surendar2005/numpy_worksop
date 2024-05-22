99a9ceb45f9a203b471fb34ecc551171ffb8364c
Surendar2005 <162149722+Surendar2005@users.noreply.github.com>
2024-05-22 10:14:32 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,10 @@
 #write a program to find the sum of digits of a given number'
+def sum_of_digits(num):
+num_str=str(num)
+digit_sum=0
+for digit in num_str:
+  digit_sum += int(digit)
+  return digit_sum
+num = int(input("Enter a number: "))
+sum_digits = sum_of_digits(num)
+print(f"The sum of digits of {num} is {sum_digits}.")
